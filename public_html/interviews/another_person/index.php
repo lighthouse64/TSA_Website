9d24163d050d79272d43f75372a84a1990123a05
lighthouse64 <rkim4425@student.dodea.edu>
2017-02-16 15:53:08 -0500

    ?

--- public_html/interviews/another_person/index.php
+++ public_html/interviews/another_person/index.php
@@ -27,6 +27,38 @@
 		</div>
 		<p>This will be the second interview
 	</div>
+	    <div class="container">
+	<p>
+        Q: Why do you believe it is important to reach mars?
+ 	</p>
+	<p>
+	A:
+	</p>
+	</div>
+	    <div class ="container">
+		<p>Q: </p>
+		<p>A:What will people do once they reach Mars? </p>
+	    </div>
+	    <div class="container">
+		    <p>Q: What about Mars interests you?</p>
+		    <p>A: </p>
+	</div>
+	<div class="container">
+		    <p>Q: How do you believe living on Mars will affect astronauts?</p>
+		    <p>A: </p>
+	</div>
+	<div class="container">
+		    <p>Q: How do you plan to supply the crew?</p>
+		    <p>A: </p>
+	</div>
+	<div class="container">
+		    <p>Q: What type of training will be required to explore Mars?</p>
+		    <p>A: </p>
+	</div>
+	<div class="container">
+		    <p>Q: How will you prepare for accidents that could occur on Mars?</p>
+		    <p>A: </p>
+	</div>
    <?php include("../../../includes/footer.php"); ?> 
     </body>
 </html>
