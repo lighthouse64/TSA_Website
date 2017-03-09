31b7a5e5f7a43eb877fe5b96350de6bd895e8fe8
DarinMao <maodarin@gmail.com>
2017-03-09 18:27:30 -0500

    Finish resources page
    We need to get the SCC filled out and linked (VERY IMPORTANT)

--- public_html/resources/index.php
+++ public_html/resources/index.php
@@ -16,6 +16,20 @@
 		<meta charset="utf-8">
 		<title>Mission To Mars</title>	
 		<?php include("../../includes/head.php"); ?>
+		<style type="text/css">
+			div.panel-body {
+				color: black;
+			}
+			img.fileicon {
+				float: left;
+				width: 10%;
+			}
+			span.filelabel {
+				float: right;
+				width: 85%;
+				font-size: 2em;
+			}
+		</style>
     </head>
     <body>
 	<?php include("../../includes/navbar.php"); ?>
@@ -25,7 +39,20 @@
 				Resources
 			</h2>
 		</div>
-		<p>This will be the resources page.
+		<p>The resources used in this website can be found adjacent to the corresponding information. </p>
+		<p>Click the links below to download a copy of our Student Copyright Checklist and Plan of Work Log. </p>
+		<div class="panel panel-default">
+			<a href="#"><div class="panel-body">
+				<img src="/images/pdf.jpg" class="fileicon">
+				<span class="filelabel">(SCC GOES HERE)</span>
+			</div></a>
+		</div>
+		<div class="panel panel-default">
+			<a href="https://docs.google.com/document/d/1H64WBwB7rC4tFPVkzgN2e6mWpAsdArzRBQeXNx97fZ8/export?format=pdf"><div class="panel-body">
+				<img src="/images/pdf.jpg" class="fileicon">
+				<span class="filelabel">Plan of Work Log</span>
+			</div></a>
+		</div>
 	</div>
 	<?php include("../../includes/footer.php"); ?>    
     </body>
