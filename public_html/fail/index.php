718cbd80f4e503ac9e3470a9fdd04b84d362477f
DarinMao <maodarin@gmail.com>
2017-03-15 17:07:04 -0400

    Procon page

--- public_html/fail/index.php
+++ public_html/fail/index.php
@@ -22,10 +22,64 @@
 	<div class="container">
 		<div class="page-header">
 			<h2>
-				What if we fail?
+				What If We Fail to Explore Mars?
 			</h2>
+			Darin Mao
 		</div>
-		<p>This will be the pros and cons of failing to explore space.
+		<p>Exploring Mars is no doubt a very challenging task, even for the talented minds over at NASA. So there is definitely some chance that we will fail to explore Mars. What will happen?</p>
+		<section>
+			<header><h3>Pros</h3></header>
+			<p>There are many downsides to not exploring Mars.</p>
+			<ol>
+				<li>
+					<h4>Survival of mankind</h4>
+					<p>Someday in the future, the human population may not be able to sustain itself with the natural resources and area available on Earth. The nuclear weapons we have on Earth could also be a large threat to humanity as a whole. On top of all of this, natural disasters such as asteroid impacts and supervolcanoes could destroy the entire planet’s population. Putting humans on Mars would ensure the survival of humankind, even if Earth becomes unsuitable for life. As SpaceX CEO Elon Musk says, "Humans need to be a multiplanet species."</p>
+				</li>
+				<li>
+					<h4>Discovering new life forms</h4>
+					<p>Robotic Mars Missions have found evidence of water on Mars, but they have yet to find solid evidence of life. Whether life exists beyond Earth remains a mystery, and putting humans on Mars could bring us closer to finding an answer.  It is likely that there is or has been life on Mars, but without a manned mission to Mars, we will never be completely sure.</p>
+				</li>
+				<li>
+					<h4>It will drive innovation</h4>
+					<p>Part of NASA’s influence lies completely outside of the space program. For example, in the early years of the Hubble Telescope, blurry pictures were taken due to a flaw in the telescope itself. Although the problem was eventually fixed, NASA tried to make use of the blurry images. The algorithms they developed to extract data from these images was applied to X-ray images, and was used to detect breast cancer. The technological advances required to put humans on Mars will no doubt be useful in other fields as well, and improve life on Earth as well as on Mars.</p>
+				</li>
+				<li>
+					<h4>Natural resources</h4>
+					<p>Mars has many natural resources that we can use on both Earth and Mars. By putting humans on Mars we can use the resources to our advantage, and possibly explore even further, without using any resources from Earth.</p>
+				</li>
+			</ol>
+		</section>
+		<section>
+			<header><h3>Pros</h3></header>
+			<p>However, there are also many benefits to satying on Earth and not exploring Mars.</p>
+			<ol>
+				<li>
+					<h4>It's dangerous</h4>
+					<p>There are many things that could go wrong, as well as many things that are just problems regardless of what we do. For example, Space Radiation has been shown to cause cancer, cataract formation, and death. Bone and muscle size would also diminish due to the microgravity experienced by humans on Mars.</p>
+				</li>
+				<li>
+					<h4>It's expensive</h4>
+					<p>The cost of a manned mission to Mars has been estimated to be 20+ billion dollars. This money could be spent on many other places, where the effect would be, relatively speaking, immediate.</p>
+				</li>
+				<li>
+					<h4>Robots are in many ways, just the same, or even better than humans</h4>
+					<p>Using robots the need for food, water, shelter, protection from radiation, and more would be completely eliminated. This would not only make the mission cost less, but also less dangerous. Humanoid robots have already been developed and sent to space, such as the Robonaut 2, which has been conducting research aboard the ISS since 2012.</p>
+				</li>
+				<li>
+					<h4>We have problems on Earth to deal with</h4>
+					<p>We have many problems here on Earth that require our immediate attention. Examples include poverty, climate change, and war. These issues are becoming more and more apparent in day to day life, and if left unattended, they could destroy the human population before any Martian advancements are made.</p>
+				</li>
+			</ol>
+		</section>
+		<section>
+			<header><h3>Works Cited</h3></header>
+			<ul class="sources">
+				<li>Briers, Demarcus. “Pros and Cons of a Manned Mission to Mars.” DBriers.com, 24 Apr. 2012, <a href="http://www.dbriers.com/tutorials/2011/11/pros-cons-manned-mission-to-mars/">www.dbriers.com/tutorials/2011/11/pros-cons-manned-mission-to-mars/</a>. Accessed 14 Mar. 2017.</li>
+				<li>Orwig, Jessica. “5 Undeniable Reasons Humans Need to Colonize Mars - Even Though It's Going to Cost Billions.” Business Insider, Business Insider, 21 Apr. 2015, <a href="http://www.businessinsider.com/5-undeniable-reasons-why-humans-should-go-to-mars-2015-4">www.businessinsider.com/5-undeniable-reasons-why-humans-should-go-to-mars-2015-4</a>. Accessed 14 Mar. 2017.</li>
+				<li>“Robonaut.” NASA, NASA, <a href="http://robonaut.jsc.nasa.gov/R2/">robonaut.jsc.nasa.gov/R2/</a>. Accessed 14 Mar. 2017.</li>
+				<li>Wiles, Jennifer. “Why We Explore.” NASA, NASA, 13 June 2013, <a href="http://www.nasa.gov/exploration/whyweexplore/why_we_explore_main.html#.WMiUs4WcHIU">www.nasa.gov/exploration/whyweexplore/why_we_explore_main.html#.WMiUs4WcHIU</a>. Accessed 14 Mar. 2017.</li>
+			</ul>
+		</section>
 	</div>
 <?php include("../../includes/footer.php"); ?>    
     </body>
