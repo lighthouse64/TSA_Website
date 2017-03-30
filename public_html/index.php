60979c6043099c7c4d6b0b4bcbaf418142d732a3
DarinMao <maodarin@gmail.com>
2017-03-30 16:25:11 -0400

    Privatization images

--- public_html/index.php
+++ public_html/index.php
@@ -58,7 +58,25 @@
 			</figcaption>
 		</figure>
 		<p>Ever since humans first gazed up at the stars, we’ve wondered what it’s like to be up there among them. These days, we’re closer than ever to that goal. By 2020-2025, major aerospace companies (i.e. SpaceX) and space-oriented government agencies (i.e. NASA) have predicted that there will he human footprints on the red planet. Numerous possibilities are opened up in this scenario. Chances at scientific research and colonization make this a juicy target.</p>
+		<figure class="left">
+			<img class="img-responsive" src="/images/population.png">
+			<figcaption>
+				A population curve graph.
+				<div class="image-attribution">
+					By El T [Public domain], <a href="https://commons.wikimedia.org/wiki/File%3APopulation_curve.svg">via Wikimedia Commons</a>
+				</div>
+			</figcaption>
+		</figure>
 		<p>The possibilities for scientific research are numerous. A probe or rover can only do a certain amount of experiments, and will eventually break down due to dust storms, equipment malfunction, or just general wear and tear. In short, they only have a limited time span and limited capability. A human on Mars would be able to read the instructions given to him/her, perform an almost unlimited amount of actions and projects, and, naturally, a human will likely last far longer than any robot we have available at this time. In addition, the effects of the Martian atmosphere and gravity will have interesting effects on any astronaut, thereby allowing any future missions to plan for the problems facing potential colonists. There are downsides to this, most notably that they astronauts would need a food-and-waste disposal system, for both nutrients, fecal waste, and CO2/Oxygen. Overall, however, the ability to have a human presence on Mars will signal a huge step forward in our scientific knowledge of the other planets in our solar system.</p>
+		<figure>
+			<img class="img-responsive" src="/images/mer.jpg">
+			<figcaption>
+				An image of a NASA Mars Exploration Rover
+				<div class="image-attribution">
+					By NASA/JPL/Cornell University, Maas Digital LLC [Public domain], <a href="https://commons.wikimedia.org/wiki/File%3ANASA_Mars_Rover.jpg">via Wikimedia Commons</a>
+				</div>
+			</figcaption>
+		</figure>
 		<p>As the human race continues to grow, within 50 years, the Earth’s population will be at the point of making her burst at the seams. Some population experts put the number in the range of 10 billion, while 50 years after that, it’ll be somewhere in the region of 20 billion. With a population of that size, our world will likely not be able to support itself. Colonizing the red planet is an option with high benefits. Mars’ ice caps should be able to support any available, and, on top of that, there’s plenty of space for potential habitats and settlements. The only downside to this idea is that water would need to be piped to the habitats themselves, as the ice caps themselves wouldn’t provide a strong enough foundation for housing. Overall, however, the ability to colonize the red planet could prove a literal lifesaver for a large portion of the population on Earth.</p>
 		<p>With those aside, there are several other reasons for exploration, such as a higher mutation level and fuel sources. In this website, we want you to learn as much as you can about Mars, what it is, why it’s important, and our plans to get there. So look, around, enjoy, and best of all, learn. As Buzz Aldrin once said, “Did the Pilgrims on the Mayflower sit around Plymouth Rock waiting for a return trip? They came here to settle. And that's what we should be doing on Mars.” Since we’re in this for the long run, it’s important that we prepare ourselves for the day so that we might also journey up into the stars and to the red planet.</p>
 		<section>
