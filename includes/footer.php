c965b6bc33270dc6eabb676da66465df9b90ca64
DarinMao <maodarin@gmail.com>
2017-03-30 18:18:38 -0400

    Changed background attribution

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 	<footer class="footer container-fluid">
 		Website By Ethan James, Ryan Kim, Darin Mao, and Ben Miller.
 		<br>Technology Student Association 2016 Website Design
-		<div id="background-attribution"><a href="https://www.jpl.nasa.gov/imagepolicy/">Image credit: NASA/JPL-Caltech/Cornell Univ./Arizona State Univ</a></div>
+		<div id="background-attribution"><a href="https://www.jpl.nasa.gov/spaceimages/details.php?id=pia19393" style="color: white;">Image credit: NASA/JPL-Caltech/Cornell Univ./Arizona State Univ</a> [<a href="https://www.jpl.nasa.gov/imagepolicy/" style="color: white;">Image Use Policy</a>]</div>
 	</footer>
 
