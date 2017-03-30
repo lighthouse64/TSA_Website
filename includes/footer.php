ebd47a6d71b0bc287c35cecb71a2b8d338867521
lighthouse64 <rkim4425@student.dodea.edu>
2017-03-30 15:31:23 -0400

    hr

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,5 @@
 	<footer class="footer container-fluid">
+		<hr>
 		Website By Ethan James, Ryan Kim, Darin Mao, and Ben Miller.
 		<br>Technology Student Association 2016 Website Design
 		<div id="background-attribution">Image credit: NASA/JPL-Caltech/Cornell Univ./Arizona State Univ</div>
