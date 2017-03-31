abc1794ba5e0fb6b095b1bbd1aefb1a1da7b658d
DarinMao <maodarin@gmail.com>
2017-03-31 17:57:51 -0400

    change footer year to 2016-2017

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 	<footer class="footer container-fluid">
 		Website By Ethan James, Ryan Kim, Darin Mao, and Ben Miller.
-		<br>Technology Student Association 2016 Website Design
+		<br>Technology Student Association 2016-2017 Website Design
 		<div id="background-attribution"><a href="https://www.jpl.nasa.gov/spaceimages/details.php?id=pia19393" style="color: white;">Image credit: NASA/JPL-Caltech/Cornell Univ./Arizona State Univ</a> [<a href="https://www.jpl.nasa.gov/imagepolicy/" style="color: white;">Image Use Policy</a>]</div>
 	</footer>
 
